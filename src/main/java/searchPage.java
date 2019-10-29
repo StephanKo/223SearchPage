ed20c6be6d3046ddf9f9917350d1f2c8f28a9d25
StephanKo <stdmko@gmail.com>
2019-10-29 15:18:34 +0300

    Initial commit

---
+++ src/main/java/searchPage.java
@@ -0,0 +1,10 @@
+import org.junit.Test;
+
+public class searchPage {
+    public static void main(String[] args){
+    }
+    @Test
+    public void searchPage(){
+        System.setProperty("webdriver.chrome.driver","src/main/resources/chromedriver.exe"); // путь к драйверу
+    }
+}
