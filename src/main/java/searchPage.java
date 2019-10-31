a1f4eff7029786f1022fcc51f2d70ecfa41dad64
StephanKo <stdmko@gmail.com>
2019-10-31 14:36:20 +0300

    1commit

--- src/main/java/searchPage.java
+++ src/main/java/searchPage.java
@@ -1,4 +1,8 @@
+import com.codeborne.selenide.WebDriverRunner;
 import org.junit.Test;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import java.util.concurrent.TimeUnit;
 
 public class searchPage {
     public static void main(String[] args){
@@ -6,6 +10,23 @@ public class searchPage {
     @Test
     public void searchPage(){
         System.setProperty("webdriver.chrome.driver","src/main/resources/chromedriver.exe"); // путь к драйверу
+        WebDriver driver = new ChromeDriver();// инициализация вебдрайвера
+        driver.manage().timeouts().implicitlyWait(10, TimeUnit.SECONDS);// ожидание исполнения скрипта 10 сек
+        driver.manage().window().maximize();//раскрытие окно на весь экран
+        WebDriverRunner.setWebDriver(driver);
+
+        public String getDates(){
+            return this.dates;
+        }
+
+        driver.get("https://223.rts-tender.ru/supplier/auction/Trade/Search.aspx"); // открываем страницу поиска закупок
+
+
+
+    }
+    public class dates(){
+        String startDate = "30.10.2019";
+        String endDate = "31.10.2019";
 
     }
 }
