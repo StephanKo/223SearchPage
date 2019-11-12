a22acd98733db57721dd5a6cdf5ee387a61ba981
StephanKo <stdmko@gmail.com>
2019-11-12 17:48:20 +0300

    1commit

--- src/main/java/searchPage.java
+++ src/main/java/searchPage.java
@@ -5,28 +5,23 @@ import org.openqa.selenium.chrome.ChromeDriver;
 import java.util.concurrent.TimeUnit;
 
 public class searchPage {
-    public static void main(String[] args){
-    }
-    @Test
-    public void searchPage(){
-        System.setProperty("webdriver.chrome.driver","src/main/resources/chromedriver.exe"); // путь к драйверу
-        WebDriver driver = new ChromeDriver();// инициализация вебдрайвера
-        driver.manage().timeouts().implicitlyWait(10, TimeUnit.SECONDS);// ожидание исполнения скрипта 10 сек
-        driver.manage().window().maximize();//раскрытие окно на весь экран
-        WebDriverRunner.setWebDriver(driver);
 
-        public String getDates(){
-            return this.dates;
+   public static void main(String[] args){
+    }
+        @Test
+        public void searchPage() {
+            System.setProperty("webdriver.chrome.driver", System.getProperty("user.dir")+"/src/main/resources/chromedriver.exe"); //путь к драйверу
+            WebDriver driver = new ChromeDriver();// инициализация вебдрайвера
+            driver.manage().timeouts().implicitlyWait(10, TimeUnit.SECONDS);// ожидание исполнения скрипта 10 сек
+            driver.manage().window().maximize();//раскрытие окно на весь экран
+            WebDriverRunner.setWebDriver(driver);
+
+            driver.get("https://223.rts-tender.ru/supplier/auction/Trade/Search.aspx"); // открываем страницу поиска закупок
+
         }
-
-        driver.get("https://223.rts-tender.ru/supplier/auction/Trade/Search.aspx"); // открываем страницу поиска закупок
-
-
-
-    }
-    public class dates(){
+/*    public class dates(){
         String startDate = "30.10.2019";
         String endDate = "31.10.2019";
-
+    }*/
     }
-}
+
